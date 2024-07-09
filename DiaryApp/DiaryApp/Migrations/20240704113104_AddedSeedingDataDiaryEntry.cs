0d6f28f236a4bb335c90a24f8b90a919b4b3d5da
adimitrovska <adimitrovska@tu-sofia.bg>
2024-07-09 12:37:36 +0300

    Adding DiaryApp

---
+++ DiaryApp/DiaryApp/Migrations/20240704113104_AddedSeedingDataDiaryEntry.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 
+
+namespace DiaryApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedSeedingDataDiaryEntry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "DiaryEntries",
+                columns: new[] { "Id", "Content", "Created", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Went hiking with Joe!", new DateTime(2024, 7, 4, 14, 31, 3, 224, DateTimeKind.Local).AddTicks(3526), "Went Hiking" },
+                    { 2, "Went shopping with Joe!", new DateTime(2024, 7, 4, 14, 31, 3, 224, DateTimeKind.Local).AddTicks(3950), "Went Shopping" },
+                    { 3, "Went diving with Joe!", new DateTime(2024, 7, 4, 14, 31, 3, 224, DateTimeKind.Local).AddTicks(3955), "Went Diving" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
---
+++ DiaryApp/DiaryApp/Models/DiaryEntry.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiaryApp.Models
+{
+    public class DiaryEntry 
+    {
+        public int Id { get; set; }
+        [Required (ErrorMessage="Please Enter a Title!")] 
+        [StringLength(100, MinimumLength =3, ErrorMessage ="Title must be between 3 - 100 Characters!")] 
+        public string Title { get; set; } = String.Empty; 
+        [Required]
+        [StringLength(1000,MinimumLength =10, ErrorMessage ="Content must be between 10 - 1000 Characters!")]
+        public string Content { get; set; } = String.Empty;
+        [Required (ErrorMessage = "Please enter the Date!")]
+        public DateTime Created { get; set; } = DateTime.Now;
+    }
+}
